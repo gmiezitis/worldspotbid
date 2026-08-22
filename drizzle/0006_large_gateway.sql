6de5ef9563540303adf1e76d778f725cc6c4d956
gmiezitis <g.miezitis@gmail.com>
2026-08-22 21:39:37 +0300

    Add country ownership history and border add-on

---
+++ drizzle/0006_large_gateway.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `bid_orders` ADD `colorful_border` integer DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE `bid_orders` ADD `border_addon_cents` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
+ALTER TABLE `countries` ADD `colorful_border` integer DEFAULT false NOT NULL;
